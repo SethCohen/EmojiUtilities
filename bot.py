1aeef3ca0c85e7e84590026611e06501c0c6472c
SethCohen <47002293+SethCohen@users.noreply.github.com>
2020-10-04 18:22:46 -0400

    Live db updating/cleaning done

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ import sqlite3
 import re
 import json
 
+def diff(list1, list2):
+    return (list(list(set(list1)-set(list2)) + list(set(list2)-set(list1))))
+
 def sayLongLineSplitted(text,wrap_at=2000):
     """Splits text at spaces and joins it to strings that are as long as
     possible without overshooting wrap_at.
@@ -42,7 +45,7 @@ async def display(message):
     await message.send('Available commands:\nES createdb\nES listEmojis')
 
 @client.command()
-async def listEmojis(message):
+async def listemojis(message):
     emojis_list = '';
     for emoji in message.guild.emojis:
         if emoji.is_usable():
@@ -103,5 +106,40 @@ async def on_message(message):
 
 f = open('config.json',)
 data = json.load(f)
+
+@client.event
+async def on_guild_emojis_update(guild, before, after):
+    emoji = diff(before, after)
+    strEmoji = str(emoji[0])
+    if (len(before) > len(after)):
+        print("Deleting from db...")
+        try:
+            db_path = str(guild.id) + '.sqlite'
+            db_conn = sqlite3.connect(db_path)
+            db_cursor = db_conn.cursor()
+            db_cursor.execute("""
+                DELETE from db WHERE emoji = ?
+                """, (strEmoji,))
+
+            db_conn.commit()
+            db_cursor.close()
+        except sqlite3.Error as error:
+            print("Failed to delete record from sqlite table", error)
+    elif (len(before) < len(after)):
+        print("Adding to db...")
+        try:
+            db_path = str(guild.id) + '.sqlite'
+            db_conn = sqlite3.connect(db_path)
+            db_cursor = db_conn.cursor()
+
+            db_cursor.execute("""
+                INSERT OR IGNORE INTO
+                db(emoji, occurrence)
+                VALUES(?, 0);""", (strEmoji,))
+
+            db_conn.commit()
+            db_cursor.close()
+        except sqlite3.Error as error:
+            print("Failed to add record to sqlite table", error)
 
 client.run(data['token'])
