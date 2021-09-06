be2dad4779d25d01f0a09b235b7af574436b9e7f
SethCohen <sethcohen.dev@gmail.com>
2021-09-06 14:37:25 -0400

    Added message reaction insert to db support.

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,6 +1,38 @@
+const {insertToDb} = require("../db_model");
+const {getSettingFlag} = require("../db_model");
 module.exports = {
     name: 'messageReactionAdd',
     execute(messageReaction, user) {
         console.log(`messageReactionAdd -> ${messageReaction.message}, ${messageReaction.emoji}, ${user}.`);
+
+        const implies = (p, q) => {
+            // p -> q
+            if (p) {
+                return q;
+            } else {
+                return true;
+            }
+        }
+
+        if (messageReaction.message.author.id !== messageReaction.client.user.id) {
+            if (getSettingFlag(messageReaction.message.guild.id, 'countreacts')) {
+                let guildId = messageReaction.message.guild.id
+                let personId = messageReaction.message.author.id
+                let dateTime = messageReaction.message.createdAt.toISOString().split('T')[0]
+
+                if (
+                    implies(
+                        (messageReaction.message.author.id === user.id),
+                        getSettingFlag(messageReaction.message.guild.id, 'countselfreacts')
+                    )
+                ) {
+                    messageReaction.message.guild.emojis
+                        .fetch(messageReaction.emoji.id)
+                        .then(emoji => insertToDb(guildId, emoji.id, personId, dateTime))
+                        .catch(() => {
+                        })
+                }
+            }
+        }
     },
 };
