4c291215672e5b99a3edc8086ce082f368e1250e
SethCohen <sethcohen.dev@gmail.com>
2021-09-08 02:19:33 -0400

    Fixed welcome message.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -9,7 +9,7 @@ module.exports = {
         guild.channels.fetch().then(channels => {
             let foundChannel = channels.find(channel => channel.isText() && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
             // console.log(foundChannel)
-            let myChannel = guild.channels.cache.get(guild.systemChannelId || foundChannel.id);
+            let myChannel = guild.channels.cache.get(foundChannel.id);
             myChannel.send("Hey, thanks for adding me to your server! " +
                 "\nThere's no need to do anything else, the database has been setup for you." +
                 "\nUse /help to find the list of commands. Thanks again and have a nice day!"
