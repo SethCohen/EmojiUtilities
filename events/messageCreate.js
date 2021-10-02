19db95a473cc998c6f46bfbde81e1f065d595e9b
SethCohen <sethcohen.dev@gmail.com>
2021-10-01 20:26:12 -0400

    Added better slash command permissions check.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,16 +6,21 @@ module.exports = {
     execute(message) {
         if (message.author.id !== message.client.user.id) {
 
-            if (message.content.substring(0, 2) === 'ES') {
-                message.reply('Hey! Sorry for the inconvenience! We\'ve recently converted the bot completely to slash' +
-                    ' commands (Check it out with `/help`!) to comply with Discord\'s upcoming changes.' +
-                    '\nYou can find out more about those changes at ' +
-                    '<https://support-dev.discord.com/hc/en-us/articles/4404772028055-Message-Content-Access-Deprecation-for-Verified-Bots>'+
-                    '\nAlso, this update was quite a big update so if you notice any bugs, please report' +
-                    ' them in our Support Server: https://discord.gg/XaeERFAVfb' +
-                    '\nThe server is also a good way to keep up to date with bot changelogs or make feature requests.' +
-                    '\n\n**YOU HAVE TO RE-AUTH THE BOT FOR SLASH COMMAND ACCESS! Please re-auth the bot by clicking:** ' +
-                    '\nhttps://discord.com/api/oauth2/authorize?client_id=757326308547100712&permissions=93248&scope=applications.commands%20bot')
+            try {
+                // TODO proper error check of if bot has perms to send message.
+                if (message.content.substring(0, 2) === 'ES') {
+                    message.reply('Hey! Sorry for the inconvenience! We\'ve recently converted the bot completely to slash' +
+                        ' commands (Check it out with `/help`!) to comply with Discord\'s upcoming changes.' +
+                        '\nYou can find out more about those changes at ' +
+                        '<https://support-dev.discord.com/hc/en-us/articles/4404772028055-Message-Content-Access-Deprecation-for-Verified-Bots>' +
+                        '\nAlso, this update was quite a big update so if you notice any bugs, please report' +
+                        ' them in our Support Server: https://discord.gg/XaeERFAVfb' +
+                        '\nThe server is also a good way to keep up to date with bot changelogs or make feature requests.' +
+                        '\n\n**YOU HAVE TO RE-AUTH THE BOT FOR SLASH COMMAND ACCESS! Please re-auth the bot by clicking:** ' +
+                        '\nhttps://discord.com/api/oauth2/authorize?client_id=757326308547100712&permissions=93248&scope=applications.commands%20bot')
+                }
+            } catch (error) {
+                console.log(error.toString())
             }
 
             // console.log(`messageCreate: ${message.content}, ${message.author}.`);
