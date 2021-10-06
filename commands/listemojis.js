50b4e75cf25bf6a732db2cadd12f25c04019b568
SethCohen <sethcohen.dev@gmail.com>
2021-10-06 17:46:37 -0400

    Added error check for listemojis.js

--- commands/listemojis.js
+++ commands/listemojis.js
@@ -1,25 +1,29 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const {SlashCommandBuilder} = require('@discordjs/builders');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('listemojis')
-		.setDescription('Displays all usable emotes to chat.'),
-	async execute(interaction) {
+    data: new SlashCommandBuilder()
+        .setName('listemojis')
+        .setDescription('Displays all usable emotes to chat.'),
+    async execute(interaction) {
 
-		const emojisList = [...interaction.guild.emojis.cache.map(emoji => emoji.toString()).values()]
-		let chunkSize = 27;
-		let chunks = []
-		for (let i = 0, j = emojisList.length; i < j; i += chunkSize) {
-			let chunk = emojisList.slice(i, i + chunkSize).join(' ');
-			chunks.push(chunk)
-		}
+        const emojisList = [...interaction.guild.emojis.cache.map(emoji => emoji.toString()).values()]
+        let chunkSize = 27;
+        let chunks = []
+        for (let i = 0, j = emojisList.length; i < j; i += chunkSize) {
+            let chunk = emojisList.slice(i, i + chunkSize).join(' ');
+            chunks.push(chunk)
+        }
 
-		return interaction.reply(chunks[0]).then(()=>{
-			chunks.shift()
-			for (const chunk of chunks) {
-				interaction.channel.send(chunk)
-			}
-			interaction.channel.send(`This server has a total of ${emojisList.length} emojis.`)
-		})
-	},
+        return interaction.reply(chunks[0]).then(() => {
+            chunks.shift()
+            try {
+                for (const chunk of chunks) {
+                    interaction.channel.send(chunk)
+                }
+                interaction.channel.send(`This server has a total of ${emojisList.length} emojis.`)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
 };
