5f7d12167663c73d2afc86744d987efd49c9a526
SethCohen <sethcohen.dev@gmail.com>
2021-10-26 15:40:25 -0400

    Added total emoji count usage

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,6 +1,7 @@
 const {MessageEmbed} = require("discord.js");
 const {SlashCommandBuilder, hyperlink} = require('@discordjs/builders');
 const ms = require("ms");
+const {getGetCount} = require("../db_model");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,18 +10,24 @@ module.exports = {
     execute(interaction) {
         let guildsCount = interaction.client.guilds.cache.size
         let uptime = interaction.client.uptime
+        let botCreatedDate = interaction.client.user.createdAt.toDateString()
+        let totalEmojiCount = 0
+        interaction.client.guilds.cache.each(guild => {
+            totalEmojiCount += getGetCount(guild.id, null, '0')
+        })
 
 
         const embed = new MessageEmbed().setColor('ORANGE')
         embed.setTitle(`${interaction.client.user.username}`)
             .setThumbnail(`${interaction.client.user.avatarURL()}`)
             .addFields(
-                {name: 'Guilds:', value: guildsCount.toString(), inline: true},
-                {name: 'Uptime:', value: ms(uptime), inline: true},
+                {name: 'Guilds In:', value: guildsCount.toString(), inline: true},
+                {name: 'Current Uptime:', value: ms(uptime), inline: true},
+                {name: 'Bot Created:', value: botCreatedDate, inline: true},
+                {name: 'Emoji Usages Recorded:', value: totalEmojiCount.toString(), inline: true},
             )
             .setDescription(hyperlink('Vote for Emoji Utilities!', 'https://top.gg/bot/757326308547100712'))
         // TODO add shards count when shards are implemented.
-        // TODO add total emoji counts
 
         return interaction.reply({embeds: [embed]})
 
