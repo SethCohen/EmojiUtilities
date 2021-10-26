a595c07d4b69da1e4648cab65fd23be6d7fbadb3
SethCohen <sethcohen.dev@gmail.com>
2021-10-26 18:41:06 -0400

    Added Manage Emoji slash command perms handler and other refactors

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -20,19 +20,32 @@ module.exports = {
             console.log("No channel access found. Welcome message not sent.")
         }
 
-        // Set role perms
-        let role = await guild.roles.cache.find(role => role.permissions.has(Permissions.FLAGS.ADMINISTRATOR))
-        guild.client.application?.commands.fetch()
-            .then(async commands => {
-                let configCommand = commands.find(command => command.name === 'config')
-                let resetdbCommand = commands.find(command => command.name === 'resetdb')
-                try {
-                    let permission = [{id: role.id, type: 'ROLE', permission: true}]
-                    await configCommand.permissions.add({guild: guild.id, permissions: permission})
-                    await resetdbCommand.permissions.add({guild: guild.id, permissions: permission})
-                } catch (e) {
-                    console.error(e.toString(), `for guild id ${guild.id}.`)
-                }
-            }).catch(e => console.error(e.toString()));       // Unable to fetch guild commands.
+        const adminCommands = ['config', 'resetdb']
+        const manageEmojisCommands = ['renameemoji', 'uploademoji', 'copysteal']
+
+        // Add admin commands role perm
+        guild.roles.cache.filter(role => role.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && !role.managed).each(adminRole => {
+            // guild.commands.fetch()                       // Guild commands
+            guild.client.application?.commands.fetch()   // Global commands
+                .then(async commands => {
+                    for (const name of adminCommands) {
+                        let command = await commands.find(command => command.name === name)
+                        let permission = [{id: adminRole.id, type: 'ROLE', permission: true}]
+                        await command.permissions.add({guild: guild.id, permissions: permission})
+                    }
+                }).catch(e => console.error(e.toString()));       // Unable to fetch guild commands.
+        })
+        // Add manage emojis commands role perm
+        guild.roles.cache.filter(role => role.permissions.has(Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS) && !role.managed).each(manageEmojisRole => {
+            // guild.commands.fetch()                       // Guild commands
+            guild.client.application?.commands.fetch()   // Global commands
+                .then(async commands => {
+                    for (const name of manageEmojisCommands) {
+                        let command = await commands.find(command => command.name === name)
+                        let permission = [{id: manageEmojisRole.id, type: 'ROLE', permission: true}]
+                        await command.permissions.add({guild: guild.id, permissions: permission})
+                    }
+                }).catch(e => console.error(e.toString()));       // Unable to fetch guild commands.
+        })
     },
 };
--- events/roleUpdate.js
+++ events/roleUpdate.js
@@ -3,34 +3,58 @@ const {Permissions} = require("discord.js");
 module.exports = {
     name: 'roleUpdate',
     async execute(oldRole, newRole) {
-        if (!oldRole.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && newRole.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+        const adminCommands = ['config', 'resetdb']
+        const manageEmojisCommands = ['renameemoji', 'uploademoji', 'copysteal']
+
+        // Admin checks
+        if (!oldRole.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && newRole.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && !newRole.managed) {
             // If a role was given Administrator perms...
-            newRole.client.application?.commands.fetch()
+            // newRole.guild.commands.fetch()                       // Guild commands
+            newRole.client.application?.commands.fetch()   // Global commands
                 .then(async commands => {
-                    let configCommand = commands.find(command => command.name === 'config')
-                    let resetdbCommand = commands.find(command => command.name === 'resetdb')
-                    try {
+                    for (const name of adminCommands) {
+                        let command = await commands.find(command => command.name === name)
                         let permission = [{id: newRole.id, type: 'ROLE', permission: true}]
-                        await configCommand.permissions.add({guild: newRole.guild.id, permissions: permission})
-                        await resetdbCommand.permissions.add({guild: newRole.guild.id, permissions: permission})
-                    } catch (e) {
-                        console.error(e.toString(), `for guild id ${newRole.guild.id}.`)
+                        await command.permissions.add({guild: newRole.guild.id, permissions: permission})
                     }
                 }).catch(e => console.error(e.toString()));       // Unable to fetch guild commands.
-        } else if (oldRole.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && !newRole.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-            // If a role was given Administrator perms...
-            newRole.client.application?.commands.fetch()
+        } else if (oldRole.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && !newRole.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && !newRole.managed) {
+            // If a role was removed Administrator perms...
+            // newRole.guild.commands.fetch()                       // Guild command
+            newRole.client.application?.commands.fetch()   // Global commands
                 .then(async commands => {
-                    let configCommand = commands.find(command => command.name === 'config')
-                    let resetdbCommand = commands.find(command => command.name === 'resetdb')
-                    try {
+                    for (const name of adminCommands) {
+                        let command = await commands.find(command => command.name === name)
                         let permission = [{id: newRole.id, type: 'ROLE', permission: false}]
-                        await configCommand.permissions.add({guild: newRole.guild.id, permissions: permission})
-                        await resetdbCommand.permissions.add({guild: newRole.guild.id, permissions: permission})
-                    } catch (e) {
-                        console.error(e.toString(), `for guild id ${newRole.guild.id}.`)
+                        await command.permissions.add({guild: newRole.guild.id, permissions: permission})
                     }
                 }).catch(e => console.error(e.toString()));       // Unable to fetch guild commands.
         }
+
+        // Manage Emojis check
+        if (!oldRole.permissions.has(Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS) && newRole.permissions.has(Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS) && !newRole.managed) {
+            // If a role was given Manage Emojis perms...
+            // newRole.guild.commands.fetch()                       // Guild commands
+            newRole.client.application?.commands.fetch()   // Global commands
+                .then(async commands => {
+                    for (const name of manageEmojisCommands) {
+                        let command = await commands.find(command => command.name === name)
+                        let permission = [{id: newRole.id, type: 'ROLE', permission: true}]
+                        await command.permissions.add({guild: newRole.guild.id, permissions: permission})
+                    }
+                }).catch(e => console.error(e.toString()));       // Unable to fetch guild commands.
+        } else if (oldRole.permissions.has(Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS) && !newRole.permissions.has(Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS) && !newRole.managed) {
+            // If a role was removed Manage Emojis perms...
+            // newRole.guild.commands.fetch()                       // Guild commands
+            newRole.client.application?.commands.fetch()   // Global commands
+                .then(async commands => {
+                    for (const name of manageEmojisCommands) {
+                        let command = await commands.find(command => command.name === name)
+                        let permission = [{id: newRole.id, type: 'ROLE', permission: false}]
+                        await command.permissions.add({guild: newRole.guild.id, permissions: permission})
+                    }
+                }).catch(e => console.error(e.toString()));       // Unable to fetch guild commands.
+        }
+
     },
 };
