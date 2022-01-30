7c47bd7a66ae90c334d9aafe87073b7b63ee28c1
SethCohen <sethcohen.dev@gmail.com>
2022-01-30 17:37:45 -0500

    Refactored botinfo.js

--- src/commands/botinfo.js
+++ src/commands/botinfo.js
@@ -4,6 +4,14 @@ const ms = require('ms');
 const { getGetCount } = require('../helpers/dbModel');
 const { mediaLinks } = require('../helpers/utilities');
 
+const getAllGuildsEmojiCount = (interaction) => {
+	let totalEmojiCount = 0;
+	interaction.client.guilds.cache.each(guild => {
+		totalEmojiCount += getGetCount(guild.id, null, '0');
+	});
+	return totalEmojiCount;
+};
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('botinfo')
@@ -13,13 +21,7 @@ module.exports = {
 		const uptime = interaction.client.uptime;
 		const botCreatedDate = interaction.client.user.createdAt.toDateString();
 
-		// Queries all databases for emoji usages
-		let totalEmojiCount = 0;
-		interaction.client.guilds.cache.each(guild => {
-			totalEmojiCount += getGetCount(guild.id, null, '0');
-		});
-
-		const embed = new MessageEmbed()
+		const embedSuccess = new MessageEmbed()
 			.setTitle(`${interaction.client.user.username}`)
 			.setDescription(mediaLinks)
 			.setThumbnail(`${interaction.client.user.avatarURL()}`)
@@ -27,11 +29,10 @@ module.exports = {
 				{ name: 'Guilds In:', value: guildsCount.toString(), inline: true },
 				{ name: 'Current Uptime:', value: ms(uptime), inline: true },
 				{ name: 'Bot Created:', value: botCreatedDate, inline: true },
-				{ name: 'Emoji Usages Recorded:', value: totalEmojiCount.toString(), inline: true },
+				{ name: 'Emoji Usages Recorded:', value: getAllGuildsEmojiCount(interaction).toString(), inline: true },
 			);
-		// TODO add shards count when shards are implemented.
 
-		return interaction.reply({ embeds: [embed] });
+		return interaction.reply({ embeds: [embedSuccess] });
 
 	},
 };
