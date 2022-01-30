4af5eac9aacbb3d8edb94e21072c56ceefe2eb8c
SethCohen <sethcohen.dev@gmail.com>
2022-01-30 17:37:21 -0500

    Refactored backupemojis.js

--- src/commands/backupemojis.js
+++ src/commands/backupemojis.js
@@ -5,6 +5,42 @@ const archiver = require('archiver');
 const { MessageEmbed } = require('discord.js');
 const { mediaLinks } = require('../helpers/utilities');
 
+const createZip = async (interaction) => {
+	const dirPath = `./src/temps/${interaction.guildId}_emojis`;
+	const output = fs.createWriteStream(dirPath + '.zip');
+	const archive = archiver('zip');
+
+	output.on('close', function() {
+		// console.log(archive.pointer() + ' total bytes');
+		// console.log('Archiver has been finalized and the output file descriptor has closed.');
+	});
+	archive.on('error', function(err) {
+		throw err;
+	});
+
+	archive.pipe(output);
+	const emojis = await interaction.guild.emojis.fetch();
+	for await (const emoji of emojis) {
+		const imageType = emoji[1].url.slice(-4);
+		const fileName = emoji[1].name;
+
+		const response = await axios.get(emoji[1].url, { responseType: 'arraybuffer' });
+		const buffer = Buffer.from(response.data, 'utf-8');
+
+		archive.append(buffer, { name: fileName + imageType });
+	}
+	await archive.finalize();
+
+	return dirPath + '.zip';
+};
+
+const deleteZip = (zipPath) => {
+	fs.unlink(zipPath, (err) => {
+		if (err) throw err;
+		// console.log(`${zipPath} was deleted.`);
+	});
+};
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('backupemojis')
@@ -12,43 +48,18 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply();
 
-		const dirPath = `./src/temps/${interaction.guildId}_emojis`;
-		const output = fs.createWriteStream(dirPath + '.zip');
-		const archive = archiver('zip');
+		await interaction.editReply({ content: 'Backing up emojis...' });
+		const zipPath = await createZip(interaction);
 
-		output.on('close', function() {
-			console.log(archive.pointer() + ' total bytes');
-			console.log('Archiver has been finalized and the output file descriptor has closed.');
-		});
-		archive.on('error', function(err) {
-			throw err;
-		});
-
-		await interaction.editReply({ content: 'Downloading emojis...' });
-		archive.pipe(output);
-		const emojis = await interaction.guild.emojis.fetch();
-		for await (const emoji of emojis) {
-			const imageType = emoji[1].url.slice(-4);
-			const fileName = emoji[1].name;
-
-			const response = await axios.get(emoji[1].url, { responseType: 'arraybuffer' });
-			const buffer = Buffer.from(response.data, 'utf-8');
-
-			archive.append(buffer, { name: fileName + imageType });
-		}
-		await archive.finalize();
-
-		const embed = new MessageEmbed()
+		const embedSuccess = new MessageEmbed()
 			.setDescription(`If you've enjoyed this bot so far, please consider voting for it.\nIt helps the bot grow. 🙂\n${mediaLinks}`);
+
 		await interaction.editReply({
 			content: `Done. The file below contains all the emojis from **${interaction.guild.name}**.`,
-			embeds: [embed],
-			files: [dirPath + '.zip'],
+			embeds: [embedSuccess],
+			files: [zipPath],
 		});
 
-		fs.unlink(dirPath + '.zip', (err) => {
-			if (err) throw err;
-			console.log(`${dirPath + '.zip'} was deleted.`);
-		});
+		await deleteZip(zipPath);
 	},
 };
