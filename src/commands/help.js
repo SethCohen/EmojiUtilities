b2c6f98fb2bef738d1c6a9dd5bf872d06812847d
SethCohen <sethcohen.dev@gmail.com>
2022-03-26 14:49:16 -0400

    Added `/help`

---
+++ src/commands/help.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { mediaLinks } = require('../helpers/utilities');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Prints a list of useful bot-related resources to chat.'),
+	async execute(interaction) {
+		const embed = new MessageEmbed()
+			.setTitle('Help')
+			.setDescription(mediaLinks + ' | [FAQ](https://github.com/SethCohen/EmojiUtilities/wiki#frequently-asked-questions) | [List of Supported Commands](https://github.com/SethCohen/EmojiUtilities/wiki)');
+
+		return interaction.reply({ embeds: [embed] });
+	},
+};
