fe06ce0db8d8ecde7962ab927ae2ca695a6995a2
SethCohen <sethcohen.dev@gmail.com>
2022-04-12 01:16:57 -0400

    style: changed bot status message

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setActivity('Bot Now Uses Slash Commands! Re-Auth Bot If Needed.');
+		client.user.setActivity('/help');
 	},
 };
