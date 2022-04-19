877eb1ea6ccba05d7b17049c245d9dbd8db3d77c
SethCohen <sethcohen.dev@gmail.com>
2022-04-19 14:29:40 -0400

    fix: fixed `unlockemoji` error on invalid emoji input with non-server emoji.

--- src/commands/unlockemoji.js
+++ src/commands/unlockemoji.js
@@ -24,25 +24,41 @@ module.exports = {
 		const stringEmoji = interaction.options.getString('emoji');
 		const verifiedEmoji = verifyEmojiString(stringEmoji);
 
-		const emoji = await interaction.guild.emojis.fetch(verifiedEmoji[3]);
+		try {
+			const emoji = await interaction.guild.emojis.fetch(verifiedEmoji[3]);
+			emoji.edit({ roles: [`${interaction.guild.id}`] })
+				.then(editedEmoji => {
+					const embed = new MessageEmbed()
+						.setTitle(`Unlocked ${editedEmoji} for role everyone.`)
+						.setDescription(`If you've enjoyed this bot so far, please consider voting for it.\nIt helps the bot grow. 🙂\n${mediaLinks}`);
+					return interaction.editReply({ embeds: [embed] });
 
-		emoji.edit({ roles: [`${interaction.guild.id}`] })
-			.then(editedEmoji => {
-				const embed = new MessageEmbed()
-					.setTitle(`Unlocked ${editedEmoji} for role everyone.`)
-					.setDescription(`If you've enjoyed this bot so far, please consider voting for it.\nIt helps the bot grow. 🙂\n${mediaLinks}`);
-				return interaction.editReply({ embeds: [embed] });
+				})
+				.catch(editEmojiError => {
+					switch (editEmojiError.message) {
+					case 'Missing Permissions':
+						interaction.editReply({ embeds: [sendErrorFeedback(interaction.commandName, 'Bot is missing `Manage Emojis And Stickers` permission.')] });
+						break;
+					default:
+						console.error(`Command:\n${interaction.commandName}\nError Message:\n${editEmojiError.message}\nRaw Input:\n${interaction.options.getString('emoji')}\n${interaction.options.getRole('role')}`);
+						return interaction.editReply({ embeds: [sendErrorFeedback(interaction.commandName)] });
+					}
+				});
+		}
+		catch (e) {
+			switch (e.message) {
+			case 'Unknown Emoji':
+				await interaction.editReply({
+					embeds: [sendErrorFeedback(interaction.commandName, 'Non-server emoji specified in field `emoji`.\nPlease try again with an emoji from this server.')],
+				});
+				break;
+			default:
+				console.error(`Command:\n${interaction.commandName}\nError Message:\n${e.message}\nRaw Input:\n${interaction.options.getString('emoji')}\n${interaction.options.getRole('role')}`);
+				return await interaction.editReply({ embeds: [sendErrorFeedback(interaction.commandName)] });
+			}
 
-			})
-			.catch(editEmojiError => {
-				switch (editEmojiError.message) {
-				case 'Missing Permissions':
-					interaction.editReply({ embeds: [sendErrorFeedback(interaction.commandName, 'Bot is missing `Manage Emojis And Stickers` permission.')] });
-					break;
-				default:
-					console.error(`Command:\n${interaction.commandName}\nError Message:\n${editEmojiError.message}\nRaw Input:\n${interaction.options.getString('emoji')}\n${interaction.options.getRole('role')}`);
-					return interaction.editReply({ embeds: [sendErrorFeedback(interaction.commandName)] });
-				}
-			});
-	},
+		}
+
+	}
+	,
 };
