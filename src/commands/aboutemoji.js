e9bff105891d1819c5a7140ab2114ac7cc43758d
SethCohen <sethcohen.dev@gmail.com>
2022-04-23 15:20:15 -0400

    refactor: `/aboutemoji` changed Manage Emojis error catch to still allow command output.

--- src/commands/aboutemoji.js
+++ src/commands/aboutemoji.js
@@ -13,11 +13,19 @@ module.exports = {
 				.setRequired(true)),
 	async execute(interaction) {
 		const stringEmoji = interaction.options.getString('emoji');
+		const verifiedEmoji = verifyEmojiString(stringEmoji);
+		const emoji = await interaction.guild.emojis.fetch(verifiedEmoji[3]);
+
+
+		let author;
+		try {
+			author = await emoji.fetchAuthor();
+		}
+		catch (e) {
+			author = '`N/A` - Bot is missing `Manage Emojis` permission and can\'t access emoji author.';
+		}
 
 		try {
-			const verifiedEmoji = verifyEmojiString(stringEmoji);
-			const emoji = await interaction.guild.emojis.fetch(verifiedEmoji[3]);
-			const author = await emoji.fetchAuthor();
 			const count = getEmojiTotalCount(interaction.guild.id, emoji.id);
 
 			const embedSuccess = new MessageEmbed()
@@ -33,22 +41,19 @@ module.exports = {
 			return interaction.reply({ embeds: [embedSuccess] });
 		}
 		catch (error) {
-			if (error.message.includes('Client must have Manage Emojis and Stickers permission')) {
-				return await interaction.reply({ embeds: [sendErrorFeedback(interaction.commandName, 'Bot is missing `Manage Emojis And Stickers` permission.')] });
-			}
-			else {
-				switch (error.message) {
-				case 'Cannot read properties of null (reading \'3\')':
-					await interaction.reply({ embeds: [sendErrorFeedback(interaction.commandName, 'No emoji found in `emoji`.')] });
-					break;
-				case 'Unknown Emoji':
-					await interaction.reply({ embeds: [sendErrorFeedback(interaction.commandName, 'Emoji in `emoji` is from another server.\nI can\'t get info on emojis from other servers, sorry!\n\nIf you were trying to get an emoji from another server though, try `/copysteal`. ')] });
-					break;
-				default:
-					console.error(`Command:\n${interaction.commandName}\nError Message:\n${error.message}\nRaw Input:\n${interaction.options.getString('emoji')}`);
-					return await interaction.reply({ embeds: [sendErrorFeedback(interaction.commandName)] });
-				}
+			switch (error.message) {
+			case 'Cannot read properties of null (reading \'3\')':
+				await interaction.reply({ embeds: [sendErrorFeedback(interaction.commandName, 'No emoji found in `emoji`.')] });
+				break;
+			case 'Unknown Emoji':
+				await interaction.reply({ embeds: [sendErrorFeedback(interaction.commandName, 'Emoji in `emoji` is from another server.\nI can\'t get info on emojis from other servers, sorry!\n\nIf you were trying to get an emoji from another server though, try `/copysteal`. ')] });
+				break;
+			default:
+				console.error(`Command:\n${interaction.commandName}\nError Message:\n${error.message}\nRaw Input:\n${interaction.options.getString('emoji')}`);
+				return await interaction.reply({ embeds: [sendErrorFeedback(interaction.commandName)] });
 			}
 		}
+
+
 	},
 };
