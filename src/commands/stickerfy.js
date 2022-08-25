9ca06c2cb3b388c5fe48ddee87b54fc7c48232f4
SethCohen <sethcohen.dev@gmail.com>
2022-08-25 16:57:16 -0400

    fix(stickerfy): added error catch on max numbers of stickers reached (15)

--- src/commands/stickerfy.js
+++ src/commands/stickerfy.js
@@ -25,6 +25,9 @@ const uploadSticker = (interaction, input, name, tag) => {
 			case 'Maximum number of stickers reached (5)':
 				await interaction.editReply({ embeds: [sendErrorFeedback(interaction.commandName, 'No sticker slots available in server.')] });
 				break;
+			case 'Maximum number of stickers reached (15)':
+				await interaction.editReply({ embeds: [sendErrorFeedback(interaction.commandName, 'No sticker slots available in server.')] });
+				break;
 			case 'Missing Permissions':
 				await interaction.editReply({ embeds: [sendErrorFeedback(interaction.commandName, 'Bot is missing `Manage Emojis And Stickers` permission.')] });
 				break;
