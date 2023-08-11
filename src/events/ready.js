2e5652b1a5a4b25222ee1325169e5ad7a1f848bd
SethCohen <sethcohen.dev@gmail.com>
2023-08-10 22:33:14 -0400

    style(ready): bot logs guilds count on load

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,7 +4,7 @@ export default {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
-    console.log(`Ready! Logged in as ${client.user.tag}`);
+    console.log(`Ready! Logged in as ${client.user.tag}\nGuilds: ${client.guilds.cache.size}`);
     client.user.setActivity('/help');
   },
 };
