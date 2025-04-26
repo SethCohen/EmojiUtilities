e075b5112e673f5340ce09ff3e0be5a801d5e834
SethCohen <sethcohen.dev@gmail.com>
2025-04-26 15:54:01 -0400

    refactor(listemojis.js): simplify emoji pagination logic and improve readability refactor(lockemoji.js): streamline error handling and response logic for emoji locking command

--- src/commands/listemojis.js
+++ src/commands/listemojis.js
@@ -2,19 +2,15 @@ import { navigationButtons } from '../helpers/utilities.js';
 import { SlashCommandBuilder } from 'discord.js';
 
 const createEmojisList = (interaction) => {
-  const emojisList = [...interaction.guild.emojis.cache.map((emoji) => emoji.toString()).values()];
+  const emojisList = [...interaction.guild.emojis.cache.map(emoji => emoji.toString()).values()];
   const emojisPerPage = 27;
   const pages = [];
-  for (
-    let pageIndex = 0, emojisListIndex = emojisList.length;
-    pageIndex < emojisListIndex;
-    pageIndex += emojisPerPage
-  ) {
-    const page = emojisList.slice(pageIndex, pageIndex + emojisPerPage).join(' ');
-    pages.push(page);
-  }
-  pages.push(`This server has a total of ${emojisList.length} emojis.`);
 
+  for (let i = 0; i < emojisList.length; i += emojisPerPage) {
+    pages.push(emojisList.slice(i, i + emojisPerPage).join(' '));
+  }
+
+  pages.push(`This server has a total of ${emojisList.length} emojis.`);
   return pages;
 };
 
@@ -22,57 +18,47 @@ export default {
   data: new SlashCommandBuilder()
     .setName('listemojis')
     .setDescription('Displays all usable emotes to chat.'),
-  async execute(interactionCommand) {
-    await interactionCommand.deferReply();
 
-    const pages = createEmojisList(interactionCommand);
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    const pages = createEmojisList(interaction);
     let currentPageIndex = 0;
 
-    await interactionCommand.editReply({
+    await interaction.editReply({
       content: pages[currentPageIndex],
       components: [navigationButtons(true)],
     });
 
-    // Create button listeners
-    const message = await interactionCommand.fetchReply();
+    const message = await interaction.fetchReply();
     const collector = message.createMessageComponentCollector({ time: 30000 });
-    collector.on('collect', async (interactionButton) => {
-      if (interactionButton.member === interactionCommand.member) {
-        if (interactionButton.customId === 'next' && currentPageIndex < pages.length - 1) {
-          ++currentPageIndex;
-        } else if (interactionButton.customId === 'prev' && currentPageIndex > 0) {
-          --currentPageIndex;
-        } else if (currentPageIndex === 0) {
-          currentPageIndex = pages.length - 1;
-        } else if (currentPageIndex === pages.length - 1) {
-          currentPageIndex = 0;
-        }
-        await interactionButton.update({ content: pages[currentPageIndex] });
-      } else {
-        await interactionButton.reply({
+
+    collector.on('collect', async button => {
+      if (button.user.id !== interaction.user.id) {
+        await button.reply({
           content: "You can't interact with this button. You are not the command author.",
           ephemeral: true,
         });
+        return;
       }
+
+      currentPageIndex = button.customId === 'next'
+        ? (currentPageIndex + 1) % pages.length
+        : (currentPageIndex - 1 + pages.length) % pages.length;
+
+      await button.update({ content: pages[currentPageIndex] });
     });
-    // eslint-disable-next-line no-unused-vars
-    collector.on('end', async (collected) => {
+
+    collector.on('end', async () => {
       try {
-        await interactionCommand.editReply({
+        await interaction.editReply({
           components: [navigationButtons(false)],
         });
       } catch (error) {
-        switch (error.message) {
-          case 'Unknown Message':
-            // Ignore unknown interactions (Often caused from deleted interactions).
-            break;
-          default:
-            console.error(
-              `Command:\n${interactionCommand.commandName}\nError Message:\n${error.message}`
-            );
+        if (error.message !== 'Unknown Message') {
+          console.error(`Command:\n${interaction.commandName}\nError:\n${error.stack}`);
         }
       }
-      // console.log(`Collected ${collected.size} interactions.`);
     });
   },
 };
