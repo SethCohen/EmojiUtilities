379b6f2c96258f592ce389145400aa64ead38218
SethCohen <sethcohen.dev@gmail.com>
2025-04-26 15:44:07 -0400

    fix(help.js): update interaction.deferReply to use ephemeral replies for better user experience feat(help.js): add footer to help embed for improved information presentation

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,16 +5,17 @@ export default {
   data: new SlashCommandBuilder()
     .setName('help')
     .setDescription('Prints a list of useful bot-related resources to chat.'),
+
   async execute(interaction) {
-    await interaction.deferReply();
+    await interaction.deferReply({ ephemeral: true });
 
     const embed = new EmbedBuilder()
       .setTitle('Help')
       .setDescription(
-        mediaLinks +
-          ' | [FAQ](https://github.com/SethCohen/EmojiUtilities/wiki#frequently-asked-questions) | [List of Supported Commands](https://github.com/SethCohen/EmojiUtilities/wiki)'
-      );
+        `${mediaLinks} | [FAQ](https://github.com/SethCohen/EmojiUtilities/wiki#frequently-asked-questions) | [List of Supported Commands](https://github.com/SethCohen/EmojiUtilities/wiki)`
+      )
+      .setFooter({ text: 'Thanks for using EmojiUtilities!' });
 
-    return interaction.editReply({ embeds: [embed] });
+    await interaction.editReply({ embeds: [embed] });
   },
 };
